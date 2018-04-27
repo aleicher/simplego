ba346d4f4102f628bb20e419dec158b0680cad06
Andreas Leicher <email@andreasleicher.com>
2018-04-28 00:12:45 +0200

    add main.go

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+import (
+  "net/http"
+  "strings"
+)
+func status(w http.ResponseWriter, r *http.Request) {
+  message := "OK"
+  w.Write([]byte(message))
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+  message := r.URL.Path
+  message = strings.TrimPrefix(message, "/")
+  message = "Hello " + message
+  w.Write([]byte(message))
+}
+
+func main() {
+  http.HandleFunc("/_status", status)
+  http.HandleFunc("/", sayHello)
+  if err := http.ListenAndServe(":80", nil); err != nil {
+    panic(err)
+  }
+}
